3d7068c8c33b86cd6c76a9cc49632c25b8677a0a
Vijay Anand <vijay.2007.22.06@gmail.com>
2022-01-30 10:13:56 +0530

    Completed

--- Components/CastListHorizontal.js
+++ Components/CastListHorizontal.js
@@ -12,7 +12,7 @@ export default function CastListHorizontal({CastList,Navigation}) {
         data={CastList}
         renderItem={({item})=>(
             <Ripple style={{marginRight:10,alignItems:'center'}} onPress={()=>Navigation.push("CastDetails")}>
-                       <ImageBackground source={{uri:item['image']}} style={{ width:100,height:100,borderRadius:100 ,backgroundColor:"#1a1a1a",resizeMode:'cover',overflow:'hidden'}}   imageStyle={{resizeMode: "cover", height: 145}}></ImageBackground>
+                       <ImageBackground source={{uri:item['image']}} style={{ width:100,height:100,borderRadius:100 ,backgroundColor:"#1a1a1a",resizeMode:'cover',overflow:'hidden',borderWidth:1,borderColor:state.UIColorsReducer.ForegroundColor}}   imageStyle={{resizeMode: "cover", height: 145}}></ImageBackground>
             <Text style={{color:state.UIColorsReducer.ForegroundColor,marginTop:5,textAlign:'center',maxWidth:100}} numberOfLines={2}>{item['name']}</Text>
         </Ripple>
         )}
--- Screens/AuthedScreens/Home/Screens/Home.js
+++ Screens/AuthedScreens/Home/Screens/Home.js
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React,{useEffect} from 'react';
+
 
 export default function Home() {
+
+
   return (
     <View>
       <Text>Home Screen</Text>
